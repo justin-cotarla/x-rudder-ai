2cec1ad7b4a8795c4a75e75b6a91ff318b848ecb
Derek Yu <derekyu624@hotmail.com>
2019-10-10 19:22:15 -0400

    [#13] Share move count between players (#14)
    * [#13] Share move count between players
    
    * [#13] Uncapitalize move_count in Player class
    
    * [#13] Remove can_move_token and can_place_token functions

--- test/test_human_player.py
+++ test/test_human_player.py
@@ -1,28 +1,38 @@
 from unittest import TestCase, main, mock
 from xrudderai.human_player import HumanPlayer
+from xrudderai.player import Player
 from xrudderai.place_command import PlaceCommand
 from xrudderai.move_command import MoveCommand
 
 class TestHumanPlayer(TestCase):
 
     def setUp(self):
-        self.humanplayer = HumanPlayer("x")
+        self.human_player = HumanPlayer("x")
+        Player.move_count = 30
 
     def test_take_turn_place(self):
         with mock.patch('builtins.input', return_value="p b8"):
-            assert isinstance(self.humanplayer.take_turn(), PlaceCommand)
+            assert isinstance(self.human_player.take_turn(), PlaceCommand)
 
     def test_take_turn_move(self):
         with mock.patch('builtins.input', return_value="m b8 b9"):
-            assert isinstance(self.humanplayer.take_turn(), MoveCommand)
+            assert isinstance(self.human_player.take_turn(), MoveCommand)
 
     def test_no_moves_left(self):
-        self.humanplayer.move_count = 30
-        self.humanplayer.tokens_left = 0
-        self.assertEqual(self.humanplayer.has_actions_left(), False)
+        Player.move_count = 0
+        self.human_player.tokens_left = 0
+        self.assertFalse(self.human_player.has_actions_left())
+
+    def test_uneven_actions_left(self):
+        Player.move_count = 0
+        second_player = HumanPlayer("o")
+        second_player.tokens_left = 0
+
+        self.assertFalse(second_player.has_actions_left())
+        self.assertTrue(self.human_player.has_actions_left())
 
     def test_token_symbol(self):
-        self.assertEqual(self.humanplayer.symbol, "x")
+        self.assertEqual(self.human_player.symbol, "x")
 
 if __name__ == '__main__':
     main()
--- xrudderai/player.py
+++ xrudderai/player.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 class Player(ABC):
 
+    move_count = 30
+
     def __init__(self, symbol):
         self.symbol = symbol
-        self.move_count = 0
         self.tokens_left = 15
         super().__init__()
 
@@ -16,13 +17,7 @@ class Player(ABC):
         self.tokens_left -= 1
 
     def moved_token(self):
-        self.move_count += 1
-
-    def can_place_token(self):
-        return self.tokens_left > 0
-
-    def can_move_token(self):
-        return self.move_count < 30
+        Player.move_count -= 1
 
     def has_actions_left(self):
-        return self.can_place_token() or self.can_move_token()
+        return self.tokens_left > 0 or Player.move_count > 0
