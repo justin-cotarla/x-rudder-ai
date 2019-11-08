50c67504f332b61d0eac810803afa5df344acf78
Derek Yu <derekyu624@hotmail.com>
2019-11-08 14:04:11 -0500

    Add heuristic to evaluate performance

--- xrudderai/player/ai_player.py
+++ xrudderai/player/ai_player.py
@@ -132,4 +132,84 @@ class AIPlayer(Player):
         return score
 
     def __calculate_heuristic(self, grid):
-        return random.randint(0, 100)
+        # return random.randint(0, 100)
+
+        # positive means player is winning
+        # negative means opponent is winning
+
+        heuristic = 0
+        for row in range(len(grid) - 2):
+            for column in range(len(grid[row]) - 2):
+
+                # heuristic += random.randint(0, 100)
+                # continue
+
+                # calculate heuristic for local 3x3
+
+                token_top_left = grid[row][column]
+                token_top_right = grid[row][column + 2]
+                token_middle = grid[row + 1][column + 1]
+                token_bottom_right = grid[row + 2][column + 2]
+                token_bottom_left = grid[row + 2][column]
+
+                token_middle_left = grid[row + 1][column]
+                token_middle_right = grid[row + 1][column + 2]
+
+                winning_positions = [token_top_left, token_top_right, token_middle, token_bottom_right, token_bottom_left]
+                horizontal_positions = [token_middle_left, token_middle_right]
+
+                # Step 1 - keep a count of tokens from each player in 3x3 in winning positions
+
+                winning_player_count = 0
+                winning_opponent_count = 0
+
+                for token in winning_positions:
+                    if token is None:
+                        continue
+                    if token.player != self:
+                        winning_opponent_count += 1
+                    if token.player == self:
+                        winning_player_count += 1
+
+                # Step 2 - if both player and opponent have tokens in winning positions, skip this 3x3
+
+                if winning_player_count > 0 and winning_opponent_count > 0:
+                    continue
+
+                # Step 3 - keep a count of tokens from each player in horizontal positions
+
+                horizontal_opponent_count = 0
+                horizontal_player_count = 0
+                
+                for token in horizontal_positions:
+                    if token is None:
+                        continue
+                    if token.player != self:
+                        horizontal_opponent_count += 1
+                    if token.player == self:
+                        horizontal_player_count += 1
+
+                # Step 4 - return if this is a winning 3x3
+
+                # Player win
+                if winning_player_count == 5 and horizontal_opponent_count != 2:
+                    return float('inf')
+
+                # Opponent win
+                if winning_opponent_count == 5 and horizontal_player_count != 2:
+                    return float('-inf')
+
+                # Step 5
+
+                player_heuristic = 0
+                opponent_heuristic = 0
+
+                if horizontal_opponent_count != 2:
+                    player_heuristic = (10 ** winning_player_count) + (10 ** horizontal_player_count)
+
+                if horizontal_player_count != 2:
+                    opponent_heuristic = (10 ** winning_opponent_count) + (10 ** horizontal_opponent_count)
+
+                heuristic += player_heuristic - opponent_heuristic
+
+        return heuristic
