2798e50be8472caed92e5adfe548a1bb5522432a
Justin Cotarla <justin.cotarla@gmail.com>
2019-11-20 17:10:04 -0500

    [#25] print move every turn

--- xrudderai/place_command.py
+++ xrudderai/place_command.py
@@ -4,4 +4,9 @@ class PlaceCommand(Command):
     def __init__(self, x, y):
         self.target_x = x
         self.target_y = y
-        
+
+    def __str__(self):
+        return "Place {}{}".format(
+            Command.REVERSE_LETTER_MAPPING[self.target_x],
+            self.target_y,
+        )        
